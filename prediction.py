9bc718bd191fe53e97b40d0ab8cc66af82e11b86
PRATIK-BOTHRA <74529643+PRATIK-BOTHRA@users.noreply.github.com>
2022-05-23 15:32:37 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -23,8 +23,6 @@ oov_tok = "<OOV>"
 tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 tokenizer.fit_on_texts(training_sentences)
 
-
-# dictionary where key : emotion , value : list
 encode_emotions = {
                     "Neutral": [0,"./static/assets/emoticons/neutral.png"],
                     "Positive": [1,"./static/assets/emoticons/positive.png"],
@@ -44,7 +42,6 @@ def predict(text):
     label = np.argmax(result , axis=1)
     label = int(label)
 
-    # extracting emotion and url from dictionary
     for emotion in encode_emotions:
         if encode_emotions[emotion][0]  ==  label:
             sentiment = emotion
