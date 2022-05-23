3dcdaa63f3a091019f7025695b9d43c199c2b69e
PRATIK-BOTHRA <74529643+PRATIK-BOTHRA@users.noreply.github.com>
2022-05-23 15:33:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# api listening to POST requests and predicting sentiments
 @app.route('/predict' , methods = ['POST'])
 def predict():
 
@@ -19,7 +18,6 @@ def predict():
     
     else:
 
-        # calling the predict method from prediction.py module
         sentiment , path = prediction.predict(review)
         response = {'status' : 'success',
                     'message' : 'Got it',
@@ -29,26 +27,24 @@ def predict():
     return jsonify(response)
 
 
-# Creating an API to save the review, user clicks on the Save button
+
 @app.route('/save' , methods = ['POST'])
 def save():
 
-    # extracting date , product name , review , sentiment associated from the JSOn data
+  
     date = request.json.get('date')
     product = request.json.get('product')
     review = request.json.get('review')
     sentiment = request.json.get('sentiment')
 
-    # creating a final variable seperated by commas
     data_entry = date + "," + product + "," + review + "," + sentiment
 
-    # open the file in the 'append' mode
+
     f = open('./static/assets/datafiles/data_entry.csv' , 'a')
 
-    # Log the data in the file
+
     f.write(data_entry + '\n')
 
-    # return a success message
     return jsonify({'status' : 'success' , 
                     'message' : 'Data Logged'})
 
